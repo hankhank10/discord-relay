d19301ed615a2b463628edd38d878b79066b3d83
hankhank10 <m.a.hankinson@gmail.com>
2024-04-01 19:14:50 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,38 +5,51 @@ import logging
 import discord
 from dotenv import load_dotenv, set_key
 
-load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
-CUSTOMER_URL = os.getenv('CUSTOMER_URL') + "api/webhook"
-HEARTBEAT_URL = os.getenv('CUSTOMER_URL') + "api/status"
-
 logging.basicConfig(level=logging.INFO)
 
 client = discord.Client(intents=discord.Intents.all())
 
-def check_server_status():
-    try:
-        r = requests.get(HEARTBEAT_URL)
-    except:
-        logging.error(f"Server is not responding at URL {HEARTBEAT_URL}")
-        return False
-    if r.status_code != 200:
-        logging.error(f"Status code {r.status_code} returned from server at URL {HEARTBEAT_URL}")
-        return False
-    logging.info(f"Status code {r.status_code} returned from server at URL {HEARTBEAT_URL}")
-    return True
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+CUSTOMER_URL = os.getenv('CUSTOMER_URL')
 
+webhook_suffix = "api/webhook"
+heartbeat_suffix = "api/status"
+
+webhook_url = None
+heartbeat_url = None
 
 def set_customer_url(url):
     global CUSTOMER_URL
+    global webhook_url
+    global heartbeat_url
     CUSTOMER_URL = url
+    webhook_url = CUSTOMER_URL + webhook_suffix
+    heartbeat_url = CUSTOMER_URL + heartbeat_suffix
     set_key(".env", "CUSTOMER_URL", url)
+
+set_customer_url(CUSTOMER_URL)
+
+def check_server_status():
+    try:
+        r = requests.get(heartbeat_url)
+    except:
+        logging.error(f"Server is not responding at URL {heartbeat_url}")
+        return False
+    if r.status_code != 200:
+        logging.error(f"Status code {r.status_code} returned from server at URL {heartbeat_url}")
+        return False
+    logging.info(f"Status code {r.status_code} returned from server at URL {heartbeat_url}")
+    return True
+
+
+
 
 
 @client.event
 async def on_ready():
     r = requests.post(
-        url=CUSTOMER_URL,
+        url=webhook_url,
         json={
             'event': 'connected_to_guild',
             'guild': {
@@ -53,7 +66,6 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    global CUSTOMER_URL
     message_handled = False
 
     if message.author == client.user:
@@ -72,9 +84,9 @@ async def on_message(message):
 
     if message.content.startswith("$server_status"):
         if check_server_status():
-            await message.channel.send(f"Server is up at url {HEARTBEAT_URL}")
+            await message.channel.send(f"Server is up at url {heartbeat_url}")
         else:
-            await message.channel.send(f"Server is not responding at url {HEARTBEAT_URL}")
+            await message.channel.send(f"Server is not responding at url {heartbeat_url}")
         message_handled = True
 
     message_json = {
@@ -102,12 +114,14 @@ async def on_message(message):
 
     if not message_handled:
         r = requests.post(
-            url = CUSTOMER_URL,
+            url = webhook_url,
             json = message_json
         )
 
         if r.status_code != 200:
             logging.error (f"Status code {r.status_code} returned")
+
+
 
 logging.info (f"Customer URL: {CUSTOMER_URL}")
 check_server_status()
